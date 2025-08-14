47e72ba619517ee501a096f2005a00c0313b32ba
utkarsh raj <utkarshr571@gmail.com>
2025-08-14 10:24:12 +0530

    javascript

---
+++ javscript/script.js
@@ -0,0 +1,33 @@
+// console.log("hello_world!");
+// console.log("utkarsh raj");
+
+// let a = 10;
+// let b = 15;
+// console.log("sum is:",a+b);
+
+// this is comment
+let pencilPrice = 10;
+let penPrice = 5;
+
+// let output = "the total price is:"+(pencilPrice + penPrice) + "rupees.";
+console.log(`the total price is : ${pencilPrice + penPrice} rupees.`)
+
+// ARITHEMATIC OPERATORS
+let a = 10;
+let b = 10;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+// UNARY OPERATORS
+console.log(a++)
+console.log(++a)
+
+// ASSIGNMENT OPERATORS 
+console.log(a+=b);
+console.log(a-=b);
+console.log(a/=b);
+console.log(a*=b);
