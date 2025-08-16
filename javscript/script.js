5c04902aac0fe55339f7662f50b4c00084602823
utkarsh raj <utkarshr571@gmail.com>
2025-08-16 22:39:06 +0530

    javascript

--- javscript/script.js
+++ javscript/script.js
@@ -6,28 +6,78 @@
 // console.log("sum is:",a+b);
 
 // this is comment
-let pencilPrice = 10;
-let penPrice = 5;
+// let pencilPrice = 10;
+// let penPrice = 5;
 
 // let output = "the total price is:"+(pencilPrice + penPrice) + "rupees.";
-console.log(`the total price is : ${pencilPrice + penPrice} rupees.`)
+// console.log(`the total price is : ${pencilPrice + penPrice} rupees.`)
 
 // ARITHEMATIC OPERATORS
-let a = 10;
-let b = 10;
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
-console.log(a**b);
+// let a = 10;
+// let b = 10;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
 
 // UNARY OPERATORS
-console.log(a++)
-console.log(++a)
+// console.log(a++)
+// console.log(++a)
 
 // ASSIGNMENT OPERATORS 
-console.log(a+=b);
-console.log(a-=b);
-console.log(a/=b);
-console.log(a*=b);
+// console.log(a+=b);
+// console.log(a-=b);
+// console.log(a/=b);
+// console.log(a*=b);
+
+//CONDITIONAL STATEMENT
+
+// console.log("before my if statement");
+// let age =23;
+
+// if(age >= 18){
+//     console.log("you are ready for vote");
+//     console.log("you can drive");
+// }
+// console.log("after my if statement");
+
+// let firstName = "utkarsh";
+// if(firstName == "utkarsh"){
+//     console.log(`welcome${firstName}`);
+// }
+
+// PRACTICE QUESTION
+//qs:- create a traffic light system that shows what to do based on color
+// let color = "red";
+// if(color === "red"){
+//     console.log("stop");
+// }
+// if(color === "yellow"){
+//     console.log("slow down");
+// }
+// if(color === "green"){
+//     console.log("go");
+// }
+
+// qs-2
+
+// let size = "L";
+
+// if(size === "XL"){
+//     console.log("price is rs.250");
+// }
+// else if(size === "L"){
+//     console.log("price is rs.200");
+// }
+// else if(size === "M"){
+//     console.log("price is rs.100");
+// }
+// else if(size === "S"){
+//     console.log("price is rs.50");
+// }
+// else{
+//     console.log("aap popcorn le nhi skte");
+// }
+
