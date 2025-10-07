360d913610bd7e7cb9ea426dde635b0be8c56b03
utkarsh raj <utkarshr571@gmail.com>
2025-10-07 17:47:36 +0530

    changes

--- javscript/lecture_10.js
+++ javscript/lecture_10.js
@@ -17,5 +17,25 @@
 // }   
 
 
+// let p = document.querySelector("p");
+// p.addEventListener("click",function(){
+//     console.log("you clicked on paragraph");
+
+// })
+
+let p = document.querySelector("p");
+let btn = document.querySelector("button");
+let h1 =document.querySelector("h1");
+let h2 = document.querySelector("h2");
+
+function changeColor(){
+    console.dir(this.innerText);
+    this.style.backgroundColor = "yellow";
+}
+
+btn.addEventListener("click",changeColor);
+p.addEventListener("click",changeColor);
+h1.addEventListener("click",changeColor);
+h2.addEventListener("click",changeColor);
 
 
