e976d81f39a34f7b2e07cae8b946c12471a66663
utkarsh raj <utkarshr571@gmail.com>
2025-10-07 13:56:26 +0530

    activity_10

---
+++ javscript/lecture_10.js
@@ -0,0 +1,21 @@
+// let btns = document.querySelectorAll('button');
+
+// for (btn of btns){
+//     // btn.addEventListener("click",sayHello);
+//     // btn.addEventListener("click",sayName);
+//     btn.addEventListener("dblclick",function(){
+//         console.log("you double clicked the button!!");
+//     });
+
+
+// }
+// function sayHello(){
+//     alert("Hello");
+// }
+// function sayName(){
+//     alert("welcome!! utkarsh");
+// }   
+
+
+
+
