809ebc89dfb8b11129723b9161eb9f068985136b
utkarsh raj <utkarshr571@gmail.com>
2025-10-09 10:15:50 +0530

    lecture10

--- javscript/lecture_10.js
+++ javscript/lecture_10.js
@@ -23,19 +23,48 @@
 
 // })
 
-let p = document.querySelector("p");
-let btn = document.querySelector("button");
-let h1 =document.querySelector("h1");
-let h2 = document.querySelector("h2");
+// let p = document.querySelector("p");
+// let btn = document.querySelector("button");
+// let h1 =document.querySelector("h1");
+// let h2 = document.querySelector("h2");
 
-function changeColor(){
-    console.dir(this.innerText);
-    this.style.backgroundColor = "yellow";
-}
+// function changeColor(){
+//     console.dir(this.innerText);
+//     this.style.backgroundColor = "yellow";
+// }
 
-btn.addEventListener("click",changeColor);
-p.addEventListener("click",changeColor);
-h1.addEventListener("click",changeColor);
-h2.addEventListener("click",changeColor);
+// btn.addEventListener("click",changeColor);
+// p.addEventListener("click",changeColor);
+// h1.addEventListener("click",changeColor);
+// h2.addEventListener("click",changeColor);
+
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click",function(event){
+//     console.log(event);
+//     console.log("button clicked!!");
+// })
+
+// KEYBOARDS EVENTS
+
+// let inp = document.querySelector("input");
+// inp.addEventListener("keydown",function(){
+//     console.log("key was pressed!");
+// });
+
+// inp.addEventListener("keydown",function(event){
+//     console.log("code = ",event.code);
+//     if(event.code == "ArrowUp"){
+//         console.log("CHARACTER MOVE UPWARDS");
+//     }else if(event.code == "ArrowDown"){
+//         console.log("CHARACTER MOVE downwards");
+//     }else if(event.code == "ArrowLeft"){
+//         console.log("CHARACTER MOVE left");
+//     }else if(event.code == "ArrowRight"){
+//         console.log("CHARACTER MOVE right");
+//     }
+// });
+
+//FORM EVENTS
 
 
