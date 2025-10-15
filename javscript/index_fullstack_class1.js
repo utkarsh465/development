b876f1e0b9d0077f19fdba2d14998ea56b691587
utkarsh raj <utkarshr571@gmail.com>
2025-10-16 00:56:39 +0530

    javascript

---
+++ javscript/index_fullstack_class1.js
@@ -0,0 +1,29 @@
+// console.log(2+false);
+//  2
+
+// console.log(String(100));
+// 100
+
+// console.log("abc");
+//  abc
+
+// console.log(Number("abc")); 
+// NaN
+
+// "a".charCodeAt(0);
+// 97
+
+// "A".charCodeAt(0);
+// 65   
+
+// String.fromCharCode(97);
+// 'b'
+
+// console.log(0/0);
+// NaN
+
+// console.log(2/0);
+// Infinity
+
+// console.log(typeof 2/0);
+// NaN
