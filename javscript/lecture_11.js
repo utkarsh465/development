1f2e0a07a077a9ff0d47091e1bd8dbcfa0507aea
utkarsh raj <utkarshr571@gmail.com>
2025-11-04 23:16:23 +0530

    js

--- javscript/lecture_11.js
+++ javscript/lecture_11.js
@@ -163,3 +163,41 @@
 // }
 // demo();
 
+// FIRST REQUEST USING FETCH API 
+// fetch(url)
+
+let url  = "https://catfact.ninja/fact";
+
+// fetch(url)
+//     .then((res)=>{
+//         return res.json();
+//     })
+//     .then((data)=>{
+//         console.log("data1",data.fact);
+//         return (fetch(url));
+//     })
+//     .then((res)=>{
+//         return res.json();
+//     })
+//     .then((data2)=>{
+//         console.log("data2 = ",data2.fact);
+//     })
+//     .catch((err) =>{
+//         console.log("error occured - ",err);
+//     });
+
+// async function getFacts(){
+//     try{
+//         let res = await fetch(url);
+//         let data = await res.json();
+//         console.log("data.fact = ", data.fact);
+
+//         let res2 = await fetch(url);
+//         let data2 = await res2.json();
+//         console.log("data2.fact = ", data2.fact);
+//     }
+//     catch(err){
+//         console.log("error occured - ",err);
+//     }
+//     console.log("function getFacts execution completed");
+// }
