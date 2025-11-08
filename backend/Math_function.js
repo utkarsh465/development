7f5237e3ff1a406cb6052ad7e1d29fa03bc411e7
utkarsh raj <utkarshr571@gmail.com>
2025-11-08 08:56:05 +0530

    node

---
+++ backend/Math_function.js
@@ -0,0 +1,45 @@
+
+// case 1 for using module.exports
+
+// const sum = (a,b) => a+b;
+// const mul = (a,b) => a*b;
+// const PI = 3.14;
+// const g = 9.8;
+
+// let object = {
+//     sum:sum,
+//     mul:mul,
+//     PI:PI,
+//     g:g
+// };
+
+// module.exports = object;
+
+// case 2 for using MediaSourceHandle.export or export
+
+// const sum = (a,b) => a+b;
+// const mul = (a,b) => a*b;
+// const PI = 3.14;
+// const g = 9.8;
+
+// module.exports = {
+//     sum:sum,
+//     mul:mul,
+//     PI:PI,
+//     g:g
+// };
+
+
+// case 3 for using module.exports and exports
+
+// module.exports.sum = (a,b) => a+b;
+// module.exports.mul = (a,b) => a*b;
+// module.exports.PI = 3.14;
+// module.exports.g = 9.8;
+
+// console.log("case 4 for using only exports");
+
+// exports.sum = (a,b) => a+b;
+// exports.mul = (a,b) => a*b;
+// exports.PI = 3.14;
+// exports.g = 9.8;
--- backend/script.js
+++ backend/script.js
@@ -2,8 +2,18 @@
 // console.log("hello from script.js");
 
 
-let args = process.argv;
+// let args = process.argv;
 
-for(let i = 2; i<args.length;i++){
-    console.log("hello to",args[i]);
-}
+// for(let i = 2; i<args.length;i++){
+//     console.log("hello to",args[i]);
+// }
+
+
+// MODULE.EXPORT , REQUIRE(), EXPORTS
+
+const math = require("./Math_function.js");
+
+console.log("sum is ",math.sum(2,3));
+console.log("mul is ",math.mul(2,3));
+console.log("PI is ",math.PI);
+console.log("g is ",math.g);
