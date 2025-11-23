a4a7fed1d292a59acb56e2aa28e1bd1588515657
utkarsh raj <utkarshr571@gmail.com>
2025-11-24 00:20:38 +0530

    get&post

--- micelleneous/backend/index.js
+++ micelleneous/backend/index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 8080;
 
 app.get('/register', (req, res) => {
-    res.send('standard GET response');
+    let {user, password} = req.query;
+    res.send(`standard GET response.Welcome ${user}!`);
 });
 
 app.post('/register', (req, res) => {
