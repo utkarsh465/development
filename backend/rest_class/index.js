c7533c01fbce173b4ccf2963315c671647687031
utkarsh raj <utkarshr571@gmail.com>
2025-12-22 16:04:49 +0530

    changes

--- backend/rest_class/index.js
+++ backend/rest_class/index.js
@@ -9,12 +9,49 @@ const path = require("path");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
+
+let posts = [
+    {
+        username : "utkarsh",
+        content : "i love coding",
+    },
+
+    {
+        username : "Raj",
+        content : "hard work is important to achieve the success ",
+    },
+
+    {
+        username : "XYZ",
+        content : "i got selected for my first internship",
+    }
+];
 
 // this is kind of a route to check whether server is working or not  
-app.get("/",(req,res)=>{
-    res.send("server working well!!");
+
+// app.get("/",(req,res)=>{
+//     res.send("server working well!!");
+// });
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});
 });
+
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+
+app.post("/posts",(req,res)=>{
+    // console.log(req.body);
+    let {username,content} = req.body;
+    posts.push({username,content})
+    // res.send("post request working");
+    res.redirect("/posts");
+});
+
 
 // it is use to start the server
 
