4de69fc755fbb808a5a13340af3508299952f056
utkarsh raj <utkarshr571@gmail.com>
2026-01-13 00:44:53 +0530

    node with sql

---
+++ backend/sql_class/index.js
@@ -0,0 +1,32 @@
+const { faker } = require('@faker-js/faker');
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', 
+  database:'delta_app',
+  password:'Utkarsh@9955'      
+});
+
+try{
+  connection.query("SHOW TABLES",(err,result) =>{
+    if(err) throw err;
+    console.log(result);
+  });
+
+}
+catch(err){
+  console.log(err);
+}
+
+let getRandomUser = () => {
+  return {
+    Id: faker.string.uuid(),
+    name: faker.internet.username(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+};
+
+// console.log(getRandomUser());
