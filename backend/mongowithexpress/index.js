6010fdb539b81bd41011fcb05c97b327c3d9d8d8
utkarsh raj <utkarshr571@gmail.com>
2026-01-27 13:52:28 +0530

    mongoWithExpress

--- backend/mongowithexpress/index.js
+++ backend/mongowithexpress/index.js
@@ -3,10 +3,11 @@ const app = express();
 
 const mongoose = require('mongoose');
 const path = require("path");
-
+const method_override = require("method-override");
 // how to link style.css file to ejs file
 
 app.use(express.static(path.join(__dirname,"public")));
+app.use(method_override("_method"));
 
 const chat = require("./models/chat.js");
 
@@ -37,8 +38,11 @@ async function main() {
 // })
 
 
-app.set("views",path.join(__dirname,"view"));
+app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
+
+// data ko parse krne ke liye use krte hai
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res) =>{
     res.send("server is working");
@@ -60,6 +64,53 @@ app.get("/chats/new",(req,res) =>{
     res.render("new.ejs");
 })
 
+
+// create route
+
+app.post("/chats",async(req,res) =>{
+    let{from,to,msg} = req.body; 
+
+    let newChat = new chat({
+        from:from,
+        to:to,
+        msg:msg,
+        created_at:new Date(),
+    });
+    await newChat.save();
+    res.redirect("/chats");
+});
+
+
+// edit route
+
+app.get("/chats/:id/edit",async(req,res) =>{
+    let {id} = req.params;
+    let chats =await chat.findById(id);
+    res.render("edit.ejs",{chats});
+})
+
+// Update Route
+
+app.put("/chats/:id",async(req,res)=>{
+    let {id} = req.params;
+    let {msg:newMsg} = req.body;
+    let updatedChat =  await chat.findByIdAndUpdate(
+        id,
+        {msg:newMsg},
+        {runValidators:true,new:true}
+    );
+    console.log(updatedChat);
+    res.redirect("/chats");
+});
+
+// Destroy or Delete Route 
+
+app.delete("/chats/:id",async(req,res) =>{
+    let {id} = req.params;
+    let deleteChat = await chat.findByIdAndDelete(id);
+    console.log(deleteChat);
+    res.redirect("/chats");
+})
 
 app.listen(8080,() =>{
     console.log("Server is running on port 8080");
